8418daef22a9dd792a85c30d96efc35348d6a02e
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-12-18 21:13:17 +0600

    project

---
+++ src/Components/Todos.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import Todo from "./Todo";
+
+const Todos = (props) => {
+  return (
+    <section className="todos">
+      {props.todos.map((todo) => (
+        <Todo
+          todo={todo.todo}
+          key={todo.id}
+          id={todo.id}
+          onRemoveTodo={props.onRemoveTodo}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default Todos
